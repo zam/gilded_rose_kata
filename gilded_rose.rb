49ba0b32c8cc74649ff4c4fa3a31ef8789df9ca4
zam <zamuel@tutanota.com>
2021-03-06 13:01:16 +0800

    refactored update quality method

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,48 +1,65 @@
 def update_quality(items)
   items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
-      end
-    else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-        end
-      end
-    end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
-      item.sell_in -= 1
-    end
-    if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
+    case item_type(item)
+    when "Sulfuras"
+      # do nothing
+    when "Aged Brie"
+      if(sell_date_has_passed(item) == true)
+        (item.quality + 2) >=50 ? item.quality = 50 : item.quality += 2
       else
-        if item.quality < 50
-          item.quality += 1
-        end
+        (item.quality + 1) >=50 ? item.quality = 50 : item.quality += 1
+        item.sell_in -= 1
+      end 
+    when "Backstage pass"
+      if (11..nil) === item.sell_in
+        (item.quality + 1) >= 50 ? item.quality = 50 : item.quality += 1
+        item.sell_in -= 1
+      elsif (6..10) === item.sell_in
+        (item.quality + 2) >= 50 ? item.quality = 50 : item.quality += 2
+        item.sell_in -= 1
+      elsif (1..5) === item.sell_in
+        (item.quality + 3) >= 50 ? item.quality = 50 : item.quality += 3
+        item.sell_in -= 1
+      else
+        item.quality = 0
+      end
+    when "Conjured"
+      if(sell_date_has_passed(item) == true)
+        (item.quality - 4) <= 0 ? item.quality = 0 : item.quality -= 4
+      else
+        (item.quality - 2) <= 0 ? item.quality = 0 : item.quality -= 2
+        item.sell_in -= 1
+      end
+    when "Normal"
+      if(sell_date_has_passed(item) == true)
+        (item.quality - 2) <= 0 ? item.quality = 0 : item.quality -= 2
+      else
+        (item.quality - 1) <= 0 ? item.quality = 0 : item.quality -= 1
+        item.sell_in -= 1
       end
     end
+  end
+end
+
+def item_type(item)
+  if (item.name.include? "Sulfuras, Hand of Ragnaros" )
+    "Sulfuras"
+  elsif (item.name.include? "Aged Brie")
+    "Aged Brie"
+  elsif (item.name.include? "Backstage pass") 
+    "Backstage pass"
+  elsif (item.name.include? "Conjured")
+    "Conjured"
+  else
+    "Normal"
+  end
+end
+
+def sell_date_has_passed(item)
+  if (item.sell_in == 0)
+    true
+  else
+    false
   end
 end
 
